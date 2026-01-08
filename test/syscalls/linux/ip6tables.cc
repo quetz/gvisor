1ac3f8e4a624eb1b5bb4dee8dff7378c71b3221b
Parth Sarthi <psarthi@google.com>
2026-01-07 16:43:58 -0800

    netstack: Added check for `CAP_NET_ADMIN` for get/setsockopt.
    Based on `net/ipv[4|6]/netfilter/ip6?_tables.c`
    
    Added syscall tests for the same.
    
    PiperOrigin-RevId: 853462573

--- test/syscalls/linux/ip6tables.cc
+++ test/syscalls/linux/ip6tables.cc
@@ -13,14 +13,27 @@
 // limitations under the License.
 
 #include <linux/capability.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
 
+#include <cerrno>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
 #include <cstring>
+#include <functional>
+#include <memory>
+#include <string>
 
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
 #include "test/syscalls/linux/iptables.h"
-#include "test/util/capability_util.h"
 #include "test/util/file_descriptor.h"
+#include "test/util/linux_capability_util.h"
+#include "test/util/multiprocess_util.h"
+#include "test/util/posix_error.h"
 #include "test/util/socket_util.h"
 #include "test/util/test_util.h"
 
@@ -35,6 +48,9 @@ constexpr size_t kEmptyStandardEntrySize =
     sizeof(struct ip6t_entry) + sizeof(struct xt_standard_target);
 constexpr size_t kEmptyErrorEntrySize =
     sizeof(struct ip6t_entry) + sizeof(struct xt_error_target);
+
+using ::testing::AnyOf;
+using ::testing::Eq;
 
 TEST(IP6TablesBasic, FailSockoptNonRaw) {
   // Even if the user has CAP_NET_RAW, they shouldn't be able to use the
@@ -229,6 +245,212 @@ TEST(IP6TablesTest, InitialEntries) {
 
   free(entries);
 }
+
+struct SockOptArgs {
+  int sock;
+  int level;
+  int optname;
+  std::shared_ptr<void> optval;
+  socklen_t optlen;
+};
+
+struct RequiresCapNetAdminTestParams {
+  std::string test_name;
+  std::function<absl::StatusOr<SockOptArgs>(int sock)> generate_sockopt_args;
+};
+
+class GetSockOptRequiresCapNetAdminTest
+    : public ::testing::TestWithParam<RequiresCapNetAdminTestParams> {
+ public:
+  // GetSockOpt calls getsockopt with CAP_NET_ADMIN and returns errno on
+  // failure.
+  static int GetSockOpt(void* args_ptr) {
+    if (args_ptr == nullptr) {
+      return -1;
+    }
+    AutoCapability cap(CAP_NET_ADMIN, true);
+    SockOptArgs* args = static_cast<SockOptArgs*>(args_ptr);
+    if (getsockopt(args->sock, args->level, args->optname, args->optval.get(),
+                   &args->optlen) != 0) {
+      return errno;
+    }
+    return 0;
+  }
+};
+
+// Tests that getsockopt on iptables sockets requires CAP_NET_ADMIN.
+TEST_P(GetSockOptRequiresCapNetAdminTest, Validate) {
+  const RequiresCapNetAdminTestParams& params = GetParam();
+  SKIP_IF(!ASSERT_NO_ERRNO_AND_VALUE(HaveCapability(CAP_NET_RAW)));
+  FileDescriptor sock = ASSERT_NO_ERRNO_AND_VALUE(
+      Socket(/*family=*/AF_INET6, /*type=*/SOCK_RAW, /*protocol=*/IPPROTO_RAW));
+  absl::StatusOr<SockOptArgs> args_or_status =
+      params.generate_sockopt_args(sock.get());
+  ASSERT_EQ(args_or_status.status(), absl::OkStatus());
+  SockOptArgs& getsockopt_args = *args_or_status;
+  SockOptArgs child_getsockopt_args = getsockopt_args;
+  // Validate that the socket creator can successfully getsockopt.
+  ASSERT_EQ(GetSockOpt(&getsockopt_args), 0);
+  // Validate that another process from a different user namespace cannot
+  // getsockopt and fails with EPERM.
+  EXPECT_THAT(InForkedProcess([&child_getsockopt_args]() {
+                // unshare the user namespace to create a new user namespace.
+                ASSERT_THAT(unshare(CLONE_NEWUSER), SyscallSucceeds());
+                ASSERT_THAT(GetSockOpt(&child_getsockopt_args), Eq(EPERM));
+              }),
+              IsPosixErrorOkAndHolds(0));
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    GetSockOptRequiresCapNetAdminTests, GetSockOptRequiresCapNetAdminTest,
+    ::testing::ValuesIn<RequiresCapNetAdminTestParams>(
+        {{.test_name = "GetInfo",
+          .generate_sockopt_args =
+              [](int sock) {
+                SockOptArgs args;
+                args.sock = sock;
+                std::shared_ptr<void> optval = std::make_shared<ipt_getinfo>();
+                ipt_getinfo* info = static_cast<ipt_getinfo*>(optval.get());
+                snprintf(info->name, XT_TABLE_MAXNAMELEN, "%s", kNatTablename);
+                args.level = SOL_IPV6;
+                args.optname = IP6T_SO_GET_INFO;
+                args.optval = optval;
+                args.optlen = sizeof(ipt_getinfo);
+                return args;
+              }},
+         {.test_name = "GetEntries",
+          .generate_sockopt_args = [](int sock) -> absl::StatusOr<SockOptArgs> {
+            socklen_t get_info_optlen = sizeof(ipt_getinfo);
+            ipt_getinfo get_info;
+            snprintf(get_info.name, XT_TABLE_MAXNAMELEN, "%s", kNatTablename);
+            if (getsockopt(sock, SOL_IPV6, IP6T_SO_GET_INFO, &get_info,
+                           &get_info_optlen) < 0) {
+              return absl::InternalError("getsockopt failed");
+            }
+            socklen_t get_entries_optlen =
+                sizeof(ipt_get_entries) + get_info.size;
+            std::shared_ptr<void> optval =
+                std::make_unique<char[]>(get_entries_optlen);
+            ipt_get_entries* entries =
+                static_cast<ipt_get_entries*>(optval.get());
+            snprintf(entries->name, XT_TABLE_MAXNAMELEN, "%s", kNatTablename);
+            entries->size = get_info.size;
+            SockOptArgs get_entries_args = {
+                .sock = sock,
+                .level = SOL_IPV6,
+                .optname = IP6T_SO_GET_ENTRIES,
+                .optval = optval,
+                .optlen = get_entries_optlen,
+            };
+            return get_entries_args;
+          }},
+         {.test_name = "GetRevisionTarget",
+          .generate_sockopt_args =
+              [](int sock) {
+                std::shared_ptr<void> optval =
+                    std::make_shared<xt_get_revision>();
+                xt_get_revision* rev =
+                    static_cast<xt_get_revision*>(optval.get());
+                socklen_t rev_len = sizeof(*rev);
+                snprintf(rev->name, sizeof(rev->name), "REDIRECT");
+                rev->revision = 0;
+                return SockOptArgs{
+                    .sock = sock,
+                    .level = SOL_IPV6,
+                    .optname = IP6T_SO_GET_REVISION_TARGET,
+                    .optval = optval,
+                    .optlen = rev_len,
+                };
+              }},
+         {.test_name = "GetRevisionMatch",
+          .generate_sockopt_args =
+              [](int sock) {
+                std::shared_ptr<void> optval =
+                    std::make_shared<xt_get_revision>();
+                xt_get_revision* rev =
+                    static_cast<xt_get_revision*>(optval.get());
+                socklen_t rev_len = sizeof(*rev);
+                snprintf(rev->name, sizeof(rev->name), "tcp");
+                rev->revision = 0;
+                return SockOptArgs{
+                    .sock = sock,
+                    .level = SOL_IPV6,
+                    .optname = IP6T_SO_GET_REVISION_MATCH,
+                    .optval = optval,
+                    .optlen = rev_len,
+                };
+              }}}),
+    [](const ::testing::TestParamInfo<
+        GetSockOptRequiresCapNetAdminTest::ParamType>& info) {
+      return info.param.test_name;
+    });
+
+class SetSockOptRequiresCapNetAdminTest
+    : public ::testing::TestWithParam<RequiresCapNetAdminTestParams> {
+ public:
+  // SetSockOpt calls setsockopt with CAP_NET_ADMIN and returns errno on
+  // failure.
+  static int SetSockOpt(void* args_ptr) {
+    if (args_ptr == nullptr) {
+      return -1;
+    }
+    AutoCapability cap(CAP_NET_ADMIN, true);
+    SockOptArgs* args = static_cast<SockOptArgs*>(args_ptr);
+    if (setsockopt(args->sock, args->level, args->optname, args->optval.get(),
+                   args->optlen) != 0) {
+      return errno;
+    }
+    return 0;
+  }
+};
+
+// Tests that setsockopt on iptables sockets requires CAP_NET_ADMIN.
+TEST_P(SetSockOptRequiresCapNetAdminTest, Validate) {
+  const RequiresCapNetAdminTestParams& params = GetParam();
+  SKIP_IF(!ASSERT_NO_ERRNO_AND_VALUE(HaveCapability(CAP_NET_RAW)));
+  FileDescriptor sock = ASSERT_NO_ERRNO_AND_VALUE(
+      Socket(/*domain=*/AF_INET6, /*type=*/SOCK_RAW, /*protocol=*/IPPROTO_RAW));
+  absl::StatusOr<SockOptArgs> args_or_status =
+      params.generate_sockopt_args(sock.get());
+  ASSERT_EQ(args_or_status.status(), absl::OkStatus());
+  SockOptArgs& setsockopt_args = *args_or_status;
+  SockOptArgs child_setsockopt_args = setsockopt_args;
+  // Validate that the socket creator either succeeds or fails with EINVAL,
+  // but not with EPERM.
+  ASSERT_THAT(SetSockOpt(&setsockopt_args),
+              AnyOf(SyscallSucceeds(), Eq(EINVAL)));
+  // Validate that another process from a different user namespace cannot
+  // setsockopt and fails with EPERM.
+  EXPECT_THAT(InForkedProcess([&child_setsockopt_args]() {
+                // unshare the user namespace to create a new user namespace.
+                ASSERT_THAT(unshare(CLONE_NEWUSER), SyscallSucceeds());
+                ASSERT_THAT(SetSockOpt(&child_setsockopt_args), Eq(EPERM));
+              }),
+              IsPosixErrorOkAndHolds(0));
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    SetSockOpt, SetSockOptRequiresCapNetAdminTest,
+    ::testing::ValuesIn<RequiresCapNetAdminTestParams>(
+        {{.test_name = "SetReplace",
+          .generate_sockopt_args =
+              [](int sock) {
+                SockOptArgs args;
+                args.sock = sock;
+                std::shared_ptr<void> optval = std::make_shared<ipt_replace>();
+                ipt_replace* replace = static_cast<ipt_replace*>(optval.get());
+                snprintf(replace->name, sizeof(replace->name), "%s",
+                         kNatTablename);
+                args.level = SOL_IPV6;
+                args.optname = IP6T_SO_SET_REPLACE;
+                args.optval = optval;
+                args.optlen = sizeof(ipt_replace);
+                return args;
+              }}}),
+    [](const ::testing::TestParamInfo<
+        SetSockOptRequiresCapNetAdminTest::ParamType>& info) {
+      return info.param.test_name;
+    });
 
 }  // namespace
 
--- test/syscalls/linux/iptables.cc
+++ test/syscalls/linux/iptables.cc
@@ -27,11 +27,24 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#include <algorithm>
+#include <cerrno>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <memory>
+#include <string>
 
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
 #include "test/util/capability_util.h"
 #include "test/util/file_descriptor.h"
+#include "test/util/linux_capability_util.h"
+#include "test/util/multiprocess_util.h"
+#include "test/util/posix_error.h"
+#include "test/util/socket_util.h"
 #include "test/util/test_util.h"
 
 namespace gvisor {
@@ -45,6 +58,9 @@ constexpr size_t kEmptyStandardEntrySize =
     sizeof(struct ipt_entry) + sizeof(struct ipt_standard_target);
 constexpr size_t kEmptyErrorEntrySize =
     sizeof(struct ipt_entry) + sizeof(struct ipt_error_target);
+
+using ::testing::AnyOf;
+using ::testing::Eq;
 
 TEST(IPTablesBasic, CreateSocket) {
   SKIP_IF(!ASSERT_NO_ERRNO_AND_VALUE(HaveCapability(CAP_NET_RAW)));
@@ -168,10 +184,10 @@ void IPTablesTest::TearDown() {
   EXPECT_THAT(close(s_), SyscallSucceeds());
 }
 
-// This tests the initial state of a machine with empty iptables. We don't have
-// a guarantee that the iptables are empty when running in native, but we can
-// test that gVisor has the same initial state that a newly-booted Linux machine
-// would have.
+// This tests the initial state of a machine with empty iptables. We don't
+// have a guarantee that the iptables are empty when running in native, but we
+// can test that gVisor has the same initial state that a newly-booted Linux
+// machine would have.
 TEST_F(IPTablesTest, InitialState) {
   SKIP_IF(!IsRunningOnGvisor());
   SKIP_IF(!ASSERT_NO_ERRNO_AND_VALUE(HaveCapability(CAP_NET_RAW)));
@@ -225,8 +241,8 @@ TEST_F(IPTablesTest, InitialState) {
   ASSERT_EQ(info.size, entries->size);
   ASSERT_EQ(strcmp(entries->name, kNatTablename), 0);
 
-  // Verify that the entrytable is 4 entries with accept targets and no matches
-  // followed by a single error target.
+  // Verify that the entrytable is 4 entries with accept targets and no
+  // matches followed by a single error target.
   size_t entry_offset = 0;
   while (entry_offset < entries->size) {
     struct ipt_entry* entry = reinterpret_cast<struct ipt_entry*>(
@@ -268,6 +284,211 @@ TEST_F(IPTablesTest, InitialState) {
   free(entries);
 }
 
+struct SockOptArgs {
+  int sock;
+  int level;
+  int optname;
+  std::shared_ptr<void> optval;
+  socklen_t optlen;
+};
+
+struct RequiresCapNetAdminTestParams {
+  std::string test_name;
+  std::function<absl::StatusOr<SockOptArgs>(int sock)> generate_sockopt_args;
+};
+
+class GetSockOptRequiresCapNetAdminTest
+    : public ::testing::TestWithParam<RequiresCapNetAdminTestParams> {
+ public:
+  // GetSockOpt calls getsockopt with CAP_NET_ADMIN and returns errno on
+  // failure.
+  static int GetSockOpt(void* args_ptr) {
+    if (args_ptr == nullptr) {
+      return -1;
+    }
+    AutoCapability cap(CAP_NET_ADMIN, true);
+    SockOptArgs* args = static_cast<SockOptArgs*>(args_ptr);
+    if (getsockopt(args->sock, args->level, args->optname, args->optval.get(),
+                   &args->optlen) != 0) {
+      return errno;
+    }
+    return 0;
+  }
+};
+
+// Tests that getsockopt on iptables sockets requires CAP_NET_ADMIN.
+TEST_P(GetSockOptRequiresCapNetAdminTest, Validate) {
+  const RequiresCapNetAdminTestParams& params = GetParam();
+  SKIP_IF(!ASSERT_NO_ERRNO_AND_VALUE(HaveCapability(CAP_NET_RAW)));
+  FileDescriptor sock = ASSERT_NO_ERRNO_AND_VALUE(
+      Socket(/*family=*/AF_INET, /*type=*/SOCK_RAW, /*protocol=*/IPPROTO_RAW));
+  absl::StatusOr<SockOptArgs> args_or_status =
+      params.generate_sockopt_args(sock.get());
+  ASSERT_EQ(args_or_status.status(), absl::OkStatus());
+  SockOptArgs& getsockopt_args = *args_or_status;
+  SockOptArgs child_getsockopt_args = getsockopt_args;
+  // Validate that the socket creator can successfully getsockopt.
+  ASSERT_EQ(GetSockOpt(&getsockopt_args), 0);
+  // Validate that another process from a different user namespace cannot
+  // getsockopt and fails with EPERM.
+  EXPECT_THAT(InForkedProcess([&child_getsockopt_args]() {
+                // unshare the user namespace to create a new user namespace.
+                ASSERT_THAT(unshare(CLONE_NEWUSER), SyscallSucceeds());
+                ASSERT_THAT(GetSockOpt(&child_getsockopt_args), Eq(EPERM));
+              }),
+              IsPosixErrorOkAndHolds(0));
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    GetSockOpt, GetSockOptRequiresCapNetAdminTest,
+    ::testing::ValuesIn<RequiresCapNetAdminTestParams>(
+        {{.test_name = "GetInfo",
+          .generate_sockopt_args =
+              [](int sock) {
+                SockOptArgs args;
+                args.sock = sock;
+                std::shared_ptr<void> optval = std::make_shared<ipt_getinfo>();
+                ipt_getinfo* info = static_cast<ipt_getinfo*>(optval.get());
+                snprintf(info->name, XT_TABLE_MAXNAMELEN, "%s", kNatTablename);
+                args.level = SOL_IP;
+                args.optname = IPT_SO_GET_INFO;
+                args.optval = optval;
+                args.optlen = sizeof(ipt_getinfo);
+                return args;
+              }},
+         {.test_name = "GetEntries",
+          .generate_sockopt_args = [](int sock) -> absl::StatusOr<SockOptArgs> {
+            socklen_t get_info_optlen = sizeof(ipt_getinfo);
+            ipt_getinfo get_info;
+            snprintf(get_info.name, XT_TABLE_MAXNAMELEN, "%s", kNatTablename);
+            if (getsockopt(sock, SOL_IP, IPT_SO_GET_INFO, &get_info,
+                           &get_info_optlen) < 0) {
+              return absl::InternalError("getsockopt failed");
+            }
+            socklen_t get_entries_optlen =
+                sizeof(ipt_get_entries) + get_info.size;
+            std::shared_ptr<void> optval =
+                std::make_unique<char[]>(get_entries_optlen);
+            ipt_get_entries* entries =
+                static_cast<ipt_get_entries*>(optval.get());
+            snprintf(entries->name, XT_TABLE_MAXNAMELEN, "%s", kNatTablename);
+            entries->size = get_info.size;
+            SockOptArgs get_entries_args = {
+                .sock = sock,
+                .level = SOL_IP,
+                .optname = IPT_SO_GET_ENTRIES,
+                .optval = optval,
+                .optlen = get_entries_optlen,
+            };
+            return get_entries_args;
+          }},
+         {.test_name = "GetRevisionTarget",
+          .generate_sockopt_args =
+              [](int sock) {
+                std::shared_ptr<void> optval =
+                    std::make_shared<xt_get_revision>();
+                xt_get_revision* rev =
+                    static_cast<xt_get_revision*>(optval.get());
+                socklen_t rev_len = sizeof(*rev);
+                snprintf(rev->name, sizeof(rev->name), "REDIRECT");
+                rev->revision = 0;
+                return SockOptArgs{
+                    .sock = sock,
+                    .level = SOL_IP,
+                    .optname = IPT_SO_GET_REVISION_TARGET,
+                    .optval = optval,
+                    .optlen = rev_len,
+                };
+              }},
+         {.test_name = "GetRevisionMatch",
+          .generate_sockopt_args =
+              [](int sock) {
+                std::shared_ptr<void> optval =
+                    std::make_shared<xt_get_revision>();
+                xt_get_revision* rev =
+                    static_cast<xt_get_revision*>(optval.get());
+                socklen_t rev_len = sizeof(*rev);
+                snprintf(rev->name, sizeof(rev->name), "tcp");
+                rev->revision = 0;
+                return SockOptArgs{
+                    .sock = sock,
+                    .level = SOL_IP,
+                    .optname = IPT_SO_GET_REVISION_MATCH,
+                    .optval = optval,
+                    .optlen = rev_len,
+                };
+              }}}),
+    [](const ::testing::TestParamInfo<
+        GetSockOptRequiresCapNetAdminTest::ParamType>& info) {
+      return info.param.test_name;
+    });
+
+class SetSockOptRequiresCapNetAdminTest
+    : public ::testing::TestWithParam<RequiresCapNetAdminTestParams> {
+ public:
+  // SetSockOpt calls setsockopt with CAP_NET_ADMIN and returns errno on
+  // failure.
+  static int SetSockOpt(void* args_ptr) {
+    if (args_ptr == nullptr) {
+      return -1;
+    }
+    AutoCapability cap(CAP_NET_ADMIN, true);
+    SockOptArgs* args = static_cast<SockOptArgs*>(args_ptr);
+    if (setsockopt(args->sock, args->level, args->optname, args->optval.get(),
+                   args->optlen) != 0) {
+      return errno;
+    }
+    return 0;
+  }
+};
+
+// Tests that setsockopt on iptables sockets requires CAP_NET_ADMIN.
+TEST_P(SetSockOptRequiresCapNetAdminTest, Validate) {
+  const RequiresCapNetAdminTestParams& params = GetParam();
+  SKIP_IF(!ASSERT_NO_ERRNO_AND_VALUE(HaveCapability(CAP_NET_RAW)));
+  FileDescriptor sock = ASSERT_NO_ERRNO_AND_VALUE(
+      Socket(/*family=*/AF_INET, /*type=*/SOCK_RAW, /*protocol=*/IPPROTO_RAW));
+  absl::StatusOr<SockOptArgs> args_or_status =
+      params.generate_sockopt_args(sock.get());
+  ASSERT_EQ(args_or_status.status(), absl::OkStatus());
+  SockOptArgs& setsockopt_args = *args_or_status;
+  SockOptArgs child_setsockopt_args = setsockopt_args;
+  // Validate that the socket creator either succeeds or fails with EINVAL,
+  // but not with EPERM.
+  ASSERT_THAT(SetSockOpt(&setsockopt_args),
+              AnyOf(SyscallSucceeds(), Eq(EINVAL)));
+  // Validate that another process from a different user namespace cannot
+  // setsockopt and fails with EPERM.
+  EXPECT_THAT(InForkedProcess([&child_setsockopt_args]() {
+                // unshare the user namespace to create a new user namespace.
+                ASSERT_THAT(unshare(CLONE_NEWUSER), SyscallSucceeds());
+                ASSERT_THAT(SetSockOpt(&child_setsockopt_args), Eq(EPERM));
+              }),
+              IsPosixErrorOkAndHolds(0));
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    SetSockOpt, SetSockOptRequiresCapNetAdminTest,
+    ::testing::ValuesIn<RequiresCapNetAdminTestParams>(
+        {{.test_name = "SetReplace",
+          .generate_sockopt_args =
+              [](int sock) {
+                SockOptArgs args;
+                args.sock = sock;
+                std::shared_ptr<void> optval = std::make_shared<ipt_replace>();
+                ipt_replace* replace = static_cast<ipt_replace*>(optval.get());
+                snprintf(replace->name, sizeof(replace->name), "%s",
+                         kNatTablename);
+                args.level = SOL_IP;
+                args.optname = IPT_SO_SET_REPLACE;
+                args.optval = optval;
+                args.optlen = sizeof(ipt_replace);
+                return args;
+              }}}),
+    [](const ::testing::TestParamInfo<
+        SetSockOptRequiresCapNetAdminTest::ParamType>& info) {
+      return info.param.test_name;
+    });
 }  // namespace
 
 }  // namespace testing
